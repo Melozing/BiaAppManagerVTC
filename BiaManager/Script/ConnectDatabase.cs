4b6227f4ff6ecfab8105cb5123df50a844a27a2f
Melozing <133214187+Melozing@users.noreply.github.com>
2024-04-25 11:44:11 +0700

    feat : connect to firebase

--- BiaManager/Script/ConnectDatabase.cs
+++ BiaManager/Script/ConnectDatabase.cs
@@ -1,14 +1,36 @@
-﻿using System.Data.SqlClient;
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using System;
+using System.Data.SqlClient;
 
 namespace BiaManager.Script
 {
     class ConnectDatabase
     {
         private static string stringConnection = @"Data Source=DESKTOP-G0D14LK;Initial Catalog=db_biamanager;Integrated Security=True";
+       
+        private IFirebaseConfig firebaseConfig = new FirebaseConfig()
+        {
+            AuthSecret = "41Rmtw0Qltxh9d27BhTqCCmAVF8QcWadDcZk9KPE",
+            BasePath = "https://biilardmanager-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        };
+        IFirebaseClient firebaseClient;
 
         public static SqlConnection GetqlConnession()
         {
             return new SqlConnection(stringConnection);
+        }
+        public ConnectDatabase()
+        {
+            try
+            {
+                firebaseClient = new FireSharp.FirebaseClient(firebaseConfig);
+                MessageFuctionConstans.WarningOK("Connect Successful!");
+            }
+            catch(Exception ex)
+            {
+                MessageFuctionConstans.WarningOK(ex.Message);
+            }
         }
     }
 }
